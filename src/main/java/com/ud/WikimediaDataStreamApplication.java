28fe029f6c0333cbdc1c2dd94d5bddbf5221b9d3
udayyadav <jnvudayju@gmail.com>
2024-08-27 15:51:47 +0530

    initial commit

---
+++ src/main/java/com/ud/WikimediaDataStreamApplication.java
@@ -0,0 +1,12 @@
+package com.ud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikimediaDataStreamApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(WikimediaDataStreamApplication.class, args);
+	}
+
+}
